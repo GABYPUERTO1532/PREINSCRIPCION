13e4572cb8c111378769fcb871c0e74eaafc2e1e
Felipe Angola <angola.andresf@gmail.com>
2022-09-13 20:57:42 -0500

    cambios dashboard y limpieza

---
+++ pages/footer.php
@@ -0,0 +1,23 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2022-2025 <a href="https://juandelcorral.edu.co">juan del corrak</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+</div>
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.js"></script>
+
+
+
+</body>
+</html>
