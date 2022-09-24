7c3c7c29ecf8e74a082a5e242790e326c34c533b
GABYPUERTO1532 <88343868+GABYPUERTO1532@users.noreply.github.com>
2022-09-24 17:24:59 -0500

    routes

--- view/empleados/coordinacion_academica/index.php
+++ view/empleados/coordinacion_academica/index.php
@@ -2,7 +2,20 @@
     session_start();
     $_SESSION['titulo']="Listado Preinscripciones";
     
-    include "C://xampp/htdocs/PREINSCRIPCION/view/header.php"; 
+    include "C://xampp/htdocs/PREINSCRIPCION/view/empleados/header.php"; 
 ?>
 
-<?php include "C://xampp/htdocs/PREINSCRIPCION/view/footer.php"?>
+    <div class="card">
+        <div class="card-header">
+            Header
+        </div>
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+        <div class="card-footer text-muted">
+            Footer
+        </div>
+    </div>
+
+<?php include "C://xampp/htdocs/PREINSCRIPCION/view/empleados/footer.php"?>
