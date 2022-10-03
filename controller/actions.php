2505453097cfa4f9f8f1373c50c64e740e06052f
GABYPUERTO1532 <88343868+GABYPUERTO1532@users.noreply.github.com>
2022-10-03 14:12:52 -0500

    RELOAD

--- controller/actions.php
+++ controller/actions.php
@@ -3,22 +3,7 @@
     include "C://xampp/htdocs/PREINSCRIPCION/config/post.php";
 
     $stu_doc_num=$_GET['stu_doc_num'];
-    if(isset($stu_doc_num)){
-        $stu_doc_num="";
-    }
-
     $stu_typ=$_GET['stu_typ'];
-<<<<<<< HEAD
-<<<<<<< HEAD
-    if(isset($stu_typ)){
-        $stu_typ="";
-    }
-=======
-    echo($stu_typ);
->>>>>>> parent of 68c6211 (ARREGLOS TESTEO ANGOLA)
-=======
-    echo($stu_typ);
->>>>>>> parent of 68c6211 (ARREGLOS TESTEO ANGOLA)
     
     if(isset($_GET['action'])){
         $post_action=$_GET['action'];
@@ -69,7 +54,7 @@
             */
             $doc_num_moth=$_GET['doc_num_moth'];
             $doc_num_fath=$_GET['doc_num_fath'];
-            $doc_num_acu=$_GET['doc_num_acu'];
+            $doc_num_acu=$_GET['doc_num_acu'];  
         
             crear_actualizar("update",$stu_doc_num);
             crear_actualizar("confirmar_preinscripcion",$stu_doc_num,$doc_num_moth,$doc_num_fath,$doc_num_acu);
