68099aa760d784ac6d7803dab3ec15d2a572de6e
GABYPUERTO1532 <88343868+GABYPUERTO1532@users.noreply.github.com>
2022-10-06 12:51:04 -0500

    Actualizacion Formulario

--- controller/pdf_structure.php
+++ controller/pdf_structure.php
@@ -1,6 +1,9 @@
 <?php
     session_start();
     $pdf_info=$_SESSION['pdf_info'];
+    $año_actual=date("Y")+1;
+    $tmp=strtotime($pdf_info['dat_bir']);
+    $edad_actual=$año_actual-(date("Y",$tmp));
 
     $html_structure=("
     <!DOCTYPE html>
@@ -9,24 +12,23 @@
             <meta charset='UTF-8'>
             <meta http-equiv='X-UA-Compatible' content='IE=edge'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <title>Hola</title>
+            <title>Formulario Preinscripcion</title>
             <style>
                 table {
                     width: 100%;
-                
                 }
                 td{
                     text-align: center;
                 }
             </style>
         </head>
-        <body>
+        <body style='font-size:15px;'>
             <div style='text-align: center'>        
                 <h3>COLEGIO ITD JUAN DEL CORRAL IED</h3>
-                <h2>MATRICULA AÑO {{ matricula.ano }}</h2>
+                <h2>MATRICULA AÑO ".$año_actual."</h2>
             </div>
             
-            <table align='center' border='1px solid black' with='500px'>
+            <table align='center'border='1px solid black'> with='500px'>
                 <tbody>
                     
                     <tr>
@@ -37,9 +39,9 @@
                     </tr>
                     <tr>
                         <td>111001009583</td>
-                        <td>Cell 2x2</td>
-                        <td>{{ formulario.jornada }}</td>
-                        <td>{{ edad }}</td>
+                        <td></td>
+                        <td>Unica</td>
+                        <td>".$edad_actual."</td>
                     </tr>
                 </tbody>
             </table>
@@ -48,8 +50,7 @@
                 
             </div>
             
-
-            <table align='center' style='border: 1px black solid'>
+            <table align='center'>
                 <tbody>
                     <tr>
                         <td style='border: 1px black solid'>PRIMER APELLIDO</td>
@@ -58,15 +59,15 @@
                         <td style='border: 1px black solid'>SEGUNDO NOMBRE</td>
                     </tr>
                     <tr>
-                        <td style='border: 1px black solid'>{{ user.pri_apellido }}</td>
-                        <td style='border: 1px black solid'>{{ user.seg_apellido }}</td>
-                        <td style='border: 1px black solid'>{{ user.pri_nombre }}</td>
-                        <td style='border: 1px black solid'>{{ user.seg_nombre }}</td>
+                        <td style='border: 1px black solid'>".$pdf_info['firs_sur']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['sec_sur']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['firs_nam']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['sec_nam']."</td>
                     </tr>
                 </tbody>
             </table>
 
-            <table align='center' style='border: 1px black solid'>
+            <table align='center'>
                 <tbody>
                     <tr>
                         <td style='border: 1px black solid'>FECHA DE NAC.</td>
@@ -77,17 +78,17 @@
                         <td style='border: 1px black solid'>EXPEDIDO EN</td>
                     </tr>
                     <tr>
-                        <td style='border: 1px black solid'>{{ user.fecha_nac }}</td>
-                        <td style='border: 1px black solid'>{{ user.ciudad_nac }}</td>
-                        <td style='border: 1px black solid'>{{ user.depart_nac }}</td>
-                        <td style='border: 1px black solid'>{{ user.tipo_id }}</td>
-                        <td style='border: 1px black solid'>{{ user.identificacion }}</td>
-                        <td style='border: 1px black solid'>{{ user.ciudad_exp }}</td>
+                        <td style='border: 1px black solid'>".$pdf_info['dat_bir']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_cit']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_dep']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_doc_typ']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_doc_num']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['exp_cit']."</td>
                     </tr>
                 </tbody>
             </table>
 
-            <table align='enter'style='order: 1px black solid'
+            <table align='center'>
                 <tbody>
                     <tr>
                         <td style='border: 1px black solid'>DIRECCION</td>
@@ -95,20 +96,18 @@
                         <td style='border: 1px black solid'>LOCALIDAD</td>
                         <td style='border: 1px black solid'>ESTRATO</td>
                         <td style='border: 1px black solid'>NIVEL SISBEN</td>
-                        <td style='border: 1px black solid'>PUNTAJE</td>
                     </tr>
                     <tr>
-                        <td style='border: 1px black solid'>Cell 1x2</td>
-                        <td style='border: 1px black solid'>Cell 2x2</td>
-                        <td style='border: 1px black solid'>Cell 3x2</td>
-                        <td style='border: 1px black solid'>Cell 4x2</td>
-                        <td style='border: 1px black solid'>Cell 5x2</td>
-                        <td style='border: 1px black solid'>Cell 6x2</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_add']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_nei']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_loc']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_est']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_cat']."</td>
                     </tr>
                 </tbody>
             </table>
 
-            <table align='center' style='border: 1px black solid'>
+            <table align='center'>
                 <tbody>
                     <tr>
                         <td style='border: 1px black solid'>TELÉFONO</td>
@@ -116,13 +115,13 @@
                         <td style='border: 1px black solid'>CIRUGÍAS FRACTURAS O ENFERMEDADES</td>
                     </tr>
                     <tr>
-                        <td style='border: 1px black solid'>Cell 1x2</td>
-                        <td style='border: 1px black solid'>Cell 2x2</td>
-                        <td style='border: 1px black solid'>Cell 3x2</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_tel']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_hob']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_enf']."</td>
                     </tr>
                 </tbody>
             </table>
-            <table align='center' style='border: 1px black solid'>
+            <table align='center'>
                 <tbody>
                     <tr>
                         <td style='border: 1px black solid'>EPS</td>
@@ -132,11 +131,11 @@
                         <td style='border: 1px black solid'>RH</td>
                     </tr>
                     <tr>
-                        <td style='border: 1px black solid'>Cell 1x2</td>
-                        <td style='border: 1px black solid'>Cell 2x2</td>
-                        <td style='border: 1px black solid'>Cell 3x2</td>
-                        <td style='border: 1px black solid'>Cell 4x2</td>
-                        <td style='border: 1px black solid'>Cell 5x2</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_eps']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_ars']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['stu_ips']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['b_grp']."</td>
+                        <td style='border: 1px black solid'>".$pdf_info['rh_fact']."</td>
                     </tr>
                 </tbody>
             </table>
@@ -158,7 +157,7 @@
             </div>
             <div style='text-align: center'>        
                 <h3>INFORMACIÓN EDUCATIVA</h3>
-                
+                ".$pdf_info['stu_doc_num']."              
             </div>
             
         </body>
