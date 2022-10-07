721b900492c3d8793521c1f30ff496683bcaaadc
GABYPUERTO1532 <88343868+GABYPUERTO1532@users.noreply.github.com>
2022-10-06 20:37:15 -0500

    Formulario V1

--- controller/pdf_structure.php
+++ controller/pdf_structure.php
@@ -20,6 +20,9 @@
                 td{
                     text-align: center;
                 }
+                h3{
+                    margin: 0px 0px 0px 0px;
+                }
             </style>
         </head>
         <body style='font-size:15px;'>
@@ -32,10 +35,10 @@
                 <tbody>
                     
                     <tr>
-                        <td>DANE</td>
-                        <td>CURSO</td>
-                        <td>JORNADA</td>
-                        <td>EDAD</td>
+                        <td><b>DANE</b></td>
+                        <td><b>CURSO</b></td>
+                        <td><b>JORNADA</b></td>
+                        <td><b>EDAD</b></td>
                     </tr>
                     <tr>
                         <td>111001009583</td>
@@ -53,10 +56,10 @@
             <table align='center'>
                 <tbody>
                     <tr>
-                        <td style='border: 1px black solid'>PRIMER APELLIDO</td>
-                        <td style='border: 1px black solid'>SEGUNDO APELLIDO</td>
-                        <td style='border: 1px black solid'>PRIMER NOMBRE</td>
-                        <td style='border: 1px black solid'>SEGUNDO NOMBRE</td>
+                        <td style='border: 1px black solid'><b>PRIMER APELLIDO</b></td>
+                        <td style='border: 1px black solid'><b>SEGUNDO APELLIDO</b></td>
+                        <td style='border: 1px black solid'><b>PRIMER NOMBRE</b></td>
+                        <td style='border: 1px black solid'><b>SEGUNDO NOMBRE</b></td>
                     </tr>
                     <tr>
                         <td style='border: 1px black solid'>".$pdf_info['firs_sur']."</td>
@@ -70,12 +73,12 @@
             <table align='center'>
                 <tbody>
                     <tr>
-                        <td style='border: 1px black solid'>FECHA DE NAC.</td>
-                        <td style='border: 1px black solid'>CIUDAD</td>
-                        <td style='border: 1px black solid'>DEPARTAMENTO</td>
-                        <td style='border: 1px black solid'>TIPO DE DOC</td>
-                        <td style='border: 1px black solid'>NUMERO</td>
-                        <td style='border: 1px black solid'>EXPEDIDO EN</td>
+                        <td style='border: 1px black solid'><b>FECHA DE NAC.</b></td>
+                        <td style='border: 1px black solid'><b>CIUDAD</b></td>
+                        <td style='border: 1px black solid'><b>DEPARTAMENTO</b></td>
+                        <td style='border: 1px black solid'><b>TIPO DE DOC</b></td>
+                        <td style='border: 1px black solid'><b>NUMERO</b></td>
+                        <td style='border: 1px black solid'><b>EXPEDIDO EN</b></td>
                     </tr>
                     <tr>
                         <td style='border: 1px black solid'>".$pdf_info['dat_bir']."</td>
@@ -91,11 +94,11 @@
             <table align='center'>
                 <tbody>
                     <tr>
-                        <td style='border: 1px black solid'>DIRECCION</td>
-                        <td style='border: 1px black solid'>BARRIO</td>
-                        <td style='border: 1px black solid'>LOCALIDAD</td>
-                        <td style='border: 1px black solid'>ESTRATO</td>
-                        <td style='border: 1px black solid'>NIVEL SISBEN</td>
+                        <td style='border: 1px black solid'><b>DIRECCION</b></td>
+                        <td style='border: 1px black solid'><b>BARRIO</b></td>
+                        <td style='border: 1px black solid'><b>LOCALIDAD</b></td>
+                        <td style='border: 1px black solid'><b>ESTRATO</b></td>
+                        <td style='border: 1px black solid'><b>NIVEL SISBEN</b></td>
                     </tr>
                     <tr>
                         <td style='border: 1px black solid'>".$pdf_info['stu_add']."</td>
@@ -110,9 +113,9 @@
             <table align='center'>
                 <tbody>
                     <tr>
-                        <td style='border: 1px black solid'>TELÉFONO</td>
-                        <td style='border: 1px black solid'>AFICIONES O HOBBIES</td>
-                        <td style='border: 1px black solid'>CIRUGÍAS FRACTURAS O ENFERMEDADES</td>
+                        <td style='border: 1px black solid'><b>TELÉFONO</b></td>
+                        <td style='border: 1px black solid'><b>AFICIONES O HOBBIES</b></td>
+                        <td style='border: 1px black solid'><b>CIRUGÍAS FRACTURAS O ENFERMEDADES</b></td>
                     </tr>
                     <tr>
                         <td style='border: 1px black solid'>".$pdf_info['stu_tel']."</td>
@@ -124,11 +127,11 @@
             <table align='center'>
                 <tbody>
                     <tr>
-                        <td style='border: 1px black solid'>EPS</td>
-                        <td style='border: 1px black solid'>ARS</td>
-                        <td style='border: 1px black solid'>IPS</td>
-                        <td style='border: 1px black solid'>G. SANGUÍNEO</td>
-                        <td style='border: 1px black solid'>RH</td>
+                        <td style='border: 1px black solid'><b>EPS</b></td>
+                        <td style='border: 1px black solid'><b>ARS</b></td>
+                        <td style='border: 1px black solid'><b>IPS</b></td>
+                        <td style='border: 1px black solid'><b>G. SANGUÍNEO</b></td>
+                        <td style='border: 1px black solid'><b>RH</b></td>
                     </tr>
                     <tr>
                         <td style='border: 1px black solid'>".$pdf_info['stu_eps']."</td>
@@ -141,23 +144,214 @@
             </table>
             <div style='text-align: center'>        
                 <h3>DATOS DE LA MADRE</h3>
+                <table align='center'>
+                    <tbody>
+                        <tr>
+                            <td style='border: 1px black solid'><b>NOMBRE COMPLETO</b></td>
+                            <td style='border: 1px black solid'><b>TIPO DOCUMENTO</b></td>
+                            <td style='border: 1px black solid'><b>NUMERO DE DOCUMENTO</b></td>
+                            <td style='border: 1px black solid'><b>TELEFONO FIJO</b></td>
+                        </tr>
+                        <tr>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_nam_moth']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['doc_typ_moth']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['doc_num_moth']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_land_moth']."</td>
+                        </tr>
+                    </tbody>
+                </table>
+
                 
+                <table align='center'>
+                    <tbody>
+                        <tr>
+                            <td style='border: 1px black solid'><b>DIRECCION</b></td>
+                            <td style='border: 1px black solid'><b>OCUPACION</b></td>
+                            <td style='border: 1px black solid'><b>CORREO</b></td>
+                        </tr>
+                        <tr>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_add_moth']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['dam_ocu_moth']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_ema_moth']."</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
+                
             <div style='text-align: center'>        
                 <h3>DATOS DEL PADRE</h3>
+                <table align='center'>
+                    <tbody>
+                        <tr>
+                            <td style='border: 1px black solid'><b>NOMBRE COMPLETO</b></td>
+                            <td style='border: 1px black solid'><b>TIPO DOCUMENTO</b></td>
+                            <td style='border: 1px black solid'><b>NUMERO DE DOCUMENTO</b></td>
+                            <td style='border: 1px black solid'><b>TELEFONO FIJO</b></td>
+                        </tr>
+                        <tr>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_nam_fath']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['doc_typ_fath']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['doc_num_fath']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_land_fath']."</td>
+                        </tr>
+                    </tbody>
+                </table>
+
                 
+                <table align='center'>
+                    <tbody>
+                        <tr>
+                            <td style='border: 1px black solid'><b>DIRECCION</b></td>
+                            <td style='border: 1px black solid'><b>OCUPACION</b></td>
+                            <td style='border: 1px black solid'><b>CORREO</b></td>
+                        </tr>
+                        <tr>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_add_fath']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['dam_ocu_fath']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_ema_fath']."</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div style='text-align: center'>        
                 <h3>DATOS DEL ACUDIENTE</h3>
+                <table align='center'>
+                    <tbody>
+                        <tr>
+                            <td style='border: 1px black solid'><b>NOMBRE COMPLETO</b></td>
+                            <td style='border: 1px black solid'><b>TIPO DOCUMENTO</b></td>
+                            <td style='border: 1px black solid'><b>NUMERO DE DOCUMENTO</b></td>
+                            <td style='border: 1px black solid'><b>TELEFONO FIJO</b></td>
+                        </tr>
+                        <tr>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_nam_acu']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['doc_typ_acu']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['doc_num_acu']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_land_acu']."</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <table align='center'>
+                    <tbody>
+                        <tr>
+                            <td style='border: 1px black solid'><b>DIRECCION</b></td>
+                            <td style='border: 1px black solid'><b>OCUPACION</b></td>
+                            <td style='border: 1px black solid'><b>CORREO</b></td>
+                        </tr>
+                        <tr>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_add_acu']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['dam_ocu_acu']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['fam_ema_acu']."</td>
+                        </tr>
+                    </tbody>
+                </table>
                 
             </div>
+
             <div style='text-align: center'>        
                 <h3>OTRA INFORMACIÓN</h3>
-                
+
+                <table align='center'>
+                    <tbody>
+                        <tr>
+                            <td style='border: 1px black solid'><b>VICTIMA C/A</b></td>
+                            <td style='border: 1px black solid'><b>S. DESPLAZAMIENTO</b></td>
+                            <td style='border: 1px black solid'><b>DESVINCULADOS G. ARMADOS</b></td>
+                            <td style='border: 1px black solid'><b>HIJO DE DESMOVILIZADO</b></td>
+                        </tr>
+                        <tr>
+                            <td style='border: 1px black solid'>".$pdf_info['conf_vic']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['dis_sit']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['dis_arm_grp']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['dem_son']."</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <table align='center'>
+                    <tbody>
+                        <tr>
+                            <td style='border: 1px black solid'><b>LIMITACIONES</b></td>
+                            <td style='border: 1px black solid'><b>CAPACIDADES EXCEPCIONALES</b></td>
+                            <td style='border: 1px black solid'><b>PUNTAJE C.I.</b></td>
+                            <td style='border: 1px black solid'><b>EXAMEN DE VAL.</b></td>
+                        </tr>
+                        <tr>
+                            <td style='border: 1px black solid'>".$pdf_info['lim_stu']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['exp_cap']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['iq_sco']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['ass_test']."</td>
+                        </tr>
+                    </tbody>
+                </table>
+
             </div>
             <div style='text-align: center'>        
                 <h3>INFORMACIÓN EDUCATIVA</h3>
-                ".$pdf_info['stu_doc_num']."              
+
+                <table align='center'>
+                    <tbody>
+                        <tr>
+                            <td style='border: 1px black solid'><b>GRADO</b></td>
+                            <td style='border: 1px black solid'><b>AÑO</b></td>
+                            <td style='border: 1px black solid'><b>CIUDAD</b></td>
+                            <td style='border: 1px black solid'><b>INSTITUCION</b></td>
+                        </tr>
+                        <tr>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_grd_1']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_yea_1']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_cit_1']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_ins_1']."</td>
+                        
+                        </tr>
+
+                        <tr>
+
+                            <td style='border: 1px black solid'>".$pdf_info['sch_grd_2']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_yea_2']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_cit_2']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_ins_2']."</td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td style='border: 1px black solid'>".$pdf_info['sch_grd_3']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_yea_3']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_cit_3']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_ins_3']."</td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td style='border: 1px black solid'>".$pdf_info['sch_grd_4']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_yea_4']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_cit_4']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_ins_4']."</td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td style='border: 1px black solid'>".$pdf_info['sch_grd_5']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_yea_5']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_cit_5']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_ins_5']."</td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td style='border: 1px black solid'>".$pdf_info['sch_grd_6']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_yea_6']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_cit_6']."</td>
+                            <td style='border: 1px black solid'>".$pdf_info['sch_ins_6']."</td>
+
+                        </tr>
+                    </tbody>
+                </table>              
             </div>
             
         </body>
